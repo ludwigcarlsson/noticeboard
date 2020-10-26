33722b7eeaebe2d9620c66184e3a3394e4c6d8b3
ludwigcarlsson <zhendiac@gmail.com>
2020-10-26 15:52:32 +0100

    refactor: removed specific id's from notice and comment because of duplicate id, changed term user to account

--- src/main/java/se/experis/noticeboard/Repositories/UserRepository.java
+++ src/main/java/se/experis/noticeboard/Repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package se.experis.noticeboard.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import se.experis.noticeboard.models.User;
+import se.experis.noticeboard.models.Account;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Account, Long> {
 }
