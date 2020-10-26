d498e58097343ef5c0b9c81d4e41c4f63508f22c
ludwigcarlsson <zhendiac@gmail.com>
2020-10-26 09:56:34 +0100

    Initial commit

---
+++ src/main/java/se/experis/noticeboard/NoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package se.experis.noticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/experis/noticeboard/NoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis.noticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
