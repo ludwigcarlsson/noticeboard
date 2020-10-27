cb4e75c45a8f1f0c68399534280cad0b2b41fa80
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2020-10-27 11:53:31 +0100

    chore: server saving accountId in session keeper
    accessible through SessionKeeper.getInstance().getSessionAccountId(sessionId)

--- src/main/java/se/experis/noticeboard/Utils/SessionKeeper.java
+++ src/main/java/se/experis/noticeboard/Utils/SessionKeeper.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class SessionKeeper {
 
   private static SessionKeeper instance = null;
-  private Map<String, Long> validSessions = new HashMap<>(); // stores session id + timestamp
+  private Map<String, User> validSessions = new HashMap<>(); // stores session id + timestamp
   private final int MAX_SESSION_TIME = 10 * 60 * 1000; // 10 minutes in ms
 
   public static SessionKeeper getInstance() {
@@ -18,29 +18,55 @@ public class SessionKeeper {
 
   /**
    * Checks if session exists and if so, if existing timestamp was less than MAX_SESSION_TIME ms ago
-   * @param session id
+   * @param sessionId id
    * @param updateTimestamp whether to refresh user timestamp or not 
    * @return logged in status
    */
-  public boolean checkSession(String session, boolean updateTimestamp) {
-    var timestamp = validSessions.get(session);
-    boolean isLoggedIn = timestamp != null && System.currentTimeMillis() < timestamp + MAX_SESSION_TIME;
+  public boolean checkSession(String sessionId, boolean updateTimestamp) {
+    var user = validSessions.get(sessionId);
+
+    boolean isLoggedIn = user != null && System.currentTimeMillis() < user.timestamp + MAX_SESSION_TIME;
     if(isLoggedIn && updateTimestamp) {
-      updateSession(session);
+      updateSession(sessionId);
     }
 
     return isLoggedIn;
   }
 
-
-  public void updateSession(String session) {
-    validSessions.put(session, System.currentTimeMillis());
+  public Long getSessionAccountId(String sessionId) {
+    checkSession(sessionId, true);
+    
+    var user = validSessions.get(sessionId);
+    if(user != null) {
+      return user.accountId;
+    }
+    return null;
   }
 
-  public void removeSession(String session) {
-    if (validSessions.get(session) != null) {
-      validSessions.remove(session);
+  public void addSession(String sessionId, long accountId) {
+    validSessions.put(sessionId, new User(System.currentTimeMillis(), accountId));
+  }
+
+  public void updateSession(String sessionId) {
+    var result = validSessions.get(sessionId);
+    result.timestamp = System.currentTimeMillis();
+  }
+
+  public void removeSession(String sessionId) {
+    if (validSessions.get(sessionId) != null) {
+      validSessions.remove(sessionId);
     }
+  }
+
+}
+
+class User {
+  long timestamp;
+  long accountId;
+
+  public User(long timestamp, long id) {
+    this.timestamp = timestamp;
+    this.accountId = id;
   }
 
 }
