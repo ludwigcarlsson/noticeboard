9af004cd3f0e486e34f03610a374862eb4ae5583
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-10-29 13:58:47 +0100

    Vattenkruka fix: added footer to login page

---
+++ src/main/resources/static/js/footer.js
@@ -0,0 +1,12 @@
+document.getElementById("footer").innerHTML = "<footer class=\"page-footer font-small bg-dark fixed-bottom footer-padding\">\n" +
+    "\n" +
+    "        <!-- Copyright -->\n" +
+    "        <div class=\"footer-copyright text-center py-3 footer-text\">© 2020 Copyright:\n" +
+    "            <a href=\"https://github.com/Bumpfel\">Eric Enoksson</a>\n" +
+    "\n" +
+    "            <a href=\"https://github.com/ludwigcarlsson\">Ludwig Carlsson</a>\n" +
+    "\n" +
+    "            <a href=\"https://github.com/Vattenkruka\">Oscar Dahlquist</a>\n" +
+    "        </div>\n" +
+    "        <!-- Copyright -->\n" +
+    "</footer>"
