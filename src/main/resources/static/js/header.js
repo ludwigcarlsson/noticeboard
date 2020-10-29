45a5175533c8ee34c4014805055c34a5e7edcade
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-10-29 13:55:20 +0100

    Vattenkruka fix: refactored header in login

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,19 @@
+document.getElementById("header").innerHTML =
+    "<header>\n" +
+    "    <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark padding-nav\">\n" +
+    "        <div class=\"center-title\">\n" +
+    "            <a class=\"navbar-brand margin-nav nav-title-size\" href=\"#\">Noticeboard</a>\n" +
+    "        </div>\n" +
+    "\n" +
+    "        <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNavAltMarkup\" aria-controls=\"navbarNavAltMarkup\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
+    "            <span class=\"navbar-toggler-icon\"></span>\n" +
+    "        </button>\n" +
+    "        <div class=\"collapse navbar-collapse\" id=\"navbarNavAltMarkup\">\n" +
+    "            <div class=\"navbar-nav\">\n" +
+    "                <a class=\"nav-item nav-link active\" href=\"#\">Home <span class=\"sr-only\">(current)</span></a>\n" +
+    "                <a class=\"nav-item nav-link\" href=\"#\">Sign up</a>\n" +
+    "                <a class=\"nav-item nav-link\" href=\"#\">Sign in</a>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </nav>\n" +
+    "</header>"
