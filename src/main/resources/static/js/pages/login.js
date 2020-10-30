5521c4d2009aea9029dff82ee9c222cf5f0ae8dc
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-10-30 12:22:50 +0100

    Vattenkruka fix: Responsive login-btn

--- src/main/resources/static/js/pages/login.js
+++ src/main/resources/static/js/pages/login.js
@@ -6,27 +6,31 @@ const contentContainer = document.querySelector('#contentContainer')
 export async function render() {
     contentContainer.innerHTML = `
     <form class="login-container" id="loginForm">
+    
     <table>
+        <div class="form-group">
         <tr>
+        
             <td class="">Username:</td>
             <td>
                 <input id="userName" class="form-control input-margin" type="text" autocomplete="off"
                         placeholder="Enter username" required>
             </td>
         </tr>
+        </div>
+        <div class="form-group">
         <tr>
             <td>Password:</td>
             <td>
-                <input id="password" class="form-control input-margin btn-block" type="password"
+                <input id="password" class="form-control input-margin " type="password"
                         placeholder="Enter password" required>
             </td>
         </tr>
-        <tr>
-            <td>
-                <button class="btn btn-primary btn-placement">Login</button>
-            </td>
-        </tr>
+        </div>
     </table>
+     <div class="form-group">     
+                <button class="btn btn-primary btn-lg mb-2 btn-placement">Login</button>
+        </div>
     </form>
     `
 
