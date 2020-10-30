450cf269d1820e24c372feaf8956a026ef6e8c5e
ludwigcarlsson <zhendiac@gmail.com>
2020-10-30 15:19:10 +0100

    feat: added some extra fields to DTO and notice

--- src/main/java/se/experis/noticeboard/models/Notice.java
+++ src/main/java/se/experis/noticeboard/models/Notice.java
@@ -2,9 +2,7 @@ package se.experis.noticeboard.models;
 
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.persistence.*;
 
@@ -43,6 +41,9 @@ public class Notice {
     private String account() {
         return account.getUserName();
     }
+
+    @JsonGetter("accountId")
+    private long accountId() { return account.getId(); }
 
     @ManyToOne
     private Account account;
