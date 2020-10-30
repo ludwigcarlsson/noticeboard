f5d804d07b1b580b16c8c6d090963b438493903b
ludwigcarlsson <zhendiac@gmail.com>
2020-10-30 15:30:41 +0100

    feat: added sorting by newest to getAllNotices

--- src/main/java/se/experis/noticeboard/Repositories/NoticeRepository.java
+++ src/main/java/se/experis/noticeboard/Repositories/NoticeRepository.java
@@ -3,5 +3,8 @@ package se.experis.noticeboard.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.experis.noticeboard.models.Notice;
 
+import java.util.List;
+
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    public List<Notice> findAllByOrderByTimestampDesc();
 }
